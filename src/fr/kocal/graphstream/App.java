cafdac6c47e3957883873267719c065ecf2257ff
ALLIAUME Hugo <kocal@live.fr>
2016-01-04 14:15:53 +0100

    Ajout des classes de base

---
+++ src/fr/kocal/graphstream/App.java
@@ -0,0 +1,12 @@
+package fr.kocal.graphstream;
+
+/**
+ * Created by Hugo Alliaume on 04/01/16.
+ */
+public class App {
+
+    public static void main(String args[]) {
+
+    }
+
+}
---
+++ src/fr/kocal/graphstream/Window.java
@@ -0,0 +1,14 @@
+package fr.kocal.graphstream;
+
+import javax.swing.*;
+
+/**
+ * Created by Hugo Alliaume on 04/01/16.
+ */
+public class Window extends JFrame {
+
+    public Window() {
+
+    }
+
+}

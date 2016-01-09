b2faa33eb33b94d65b93b6338193db881cb721bb
ALLIAUME Hugo <kocal@live.fr>
2016-01-09 12:19:58 +0100

    Le thème GTK est désormais utilisé

--- src/fr/kocal/graphstream/App.java
+++ src/fr/kocal/graphstream/App.java
@@ -1,5 +1,7 @@
 package fr.kocal.graphstream;
 
+import javax.swing.*;
+
 /**
  * Created by Hugo Alliaume on 04/01/16.
  */
@@ -7,9 +9,16 @@ public class App {
 
     public static void main(String args[]) {
 
+        try {
+            UIManager.setLookAndFeel(
+                UIManager.getSystemLookAndFeelClassName()
+            );
+        }
+        catch (UnsupportedLookAndFeelException | ClassNotFoundException | IllegalAccessException | InstantiationException e) {
+            e.printStackTrace();
+        }
+
         Window window = new Window();
         window.setVisible(true);
-
     }
-
 }
